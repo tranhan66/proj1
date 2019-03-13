948cc50228fb72214efe245034ee84e06435a30d
tranhan66 <tranhan66@gmail.com>
2019-03-13 02:11:34 +0000

    2019mar13we9.10am_by_th66gm_update_nodehttps1.js

--- nodehttp1.js
+++ nodehttp1.js
@@ -1,5 +1,6 @@
 const https=require("https");
-const url="https://jsonplaceholder.typicode.com/posts/1"; 
-https.get(url,res=>{res.setEncoding("utf8");let body=’’;
-  			res.on("data",data=>{body+=data;});
-  			res.on("end",   ()=>{body=JSON.parse(body);console.log(body);});});
+const url="https://jsonplaceholder.typicode.com/posts/1";
+https.get(url,res=>{res.setEncoding("utf8");let body='';
+  			res.on('data',data=>{body+=data;});
+  			res.on('end',   ()=>{body=JSON.parse(body);console.log(body);});});
+console.log(new Date);
