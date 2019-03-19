f3ca53b5e872d6e8b6d3a683dfe16a23ad0d0d5f
tranhan66 <tranhan66@gmail.com>
2019-03-19 14:59:51 +0700

    2019mar19tue_th66gm_create_httpurl.js

---
+++ httpurl.js
@@ -0,0 +1,14 @@
+//2019mar19tue https://tutorialspoint.com/nodejs/nodejs_web_module.htm 
+var http=require('http');var url=require('url');//var fs=require('fs');
+http.createServer((req,res)=>{  
+   var pathname=url.parse(req.url).pathname;//parse req containing filename
+   console.log("req4:"+pathname);           //rd requested file content fromFS
+   fs.readFile(pathname.substr(1),(err,data)=>{
+      if(err){console.log(err);//HTTP Status: 404 : NOT FOUND    //Content Type: text/plain
+         res.writeHead(404,{'Content-Type': 'text/html'});
+      }else{//HTTP Status: 200 : OK  //Content Type: text/plain
+         res.writeHead(200,{'Content-Type': 'text/html'});	
+         res.write(data.toString());}//wr filecontent2res.body
+      res.end();});//send res.body   
+}).listen(8081);  //console will print the message
+console.log('HttpSVrunning at http://127.0.0.1:8081/');
